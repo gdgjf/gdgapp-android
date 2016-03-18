e93dd181d99ac8994571b9c319754f24ada74211
atilabraga <braga.atila@gmail.com>
2016-03-17 23:39:12 -0300

    Adding retrofit GDG api

--- app/src/main/java/com/gdg/eventmanager/api/GDGApi.java
+++ app/src/main/java/com/gdg/eventmanager/api/GDGApi.java
@@ -1,10 +1,44 @@
 package com.gdg.eventmanager.api;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import com.gdg.eventmanager.util.PreferencesConstants;
+
+import java.io.IOException;
+
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 /**
  * Created by atilabraga on 3/15/16.
  */
 public class GDGApi {
 
     public static final String BASE_URL = "http://emjuizdefora.com/gdgjf/api/";
+
+    private static GDGApi sInstance;
+    private Retrofit mRetrofit;
+
+    private GDGApi() {
+        mRetrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+    }
+
+    public static void init() {
+        if (sInstance == null) {
+            sInstance = new GDGApi();
+        }
+    }
+
+    public static GDGApi getInstance() {
+        return sInstance;
+    }
 
 }
---
+++ app/src/main/java/com/gdg/eventmanager/util/PreferencesConstants.java
@@ -0,0 +1,12 @@
+package com.gdg.eventmanager.util;
+
+/**
+ * Created by atilabraga on 3/17/16.
+ */
+public abstract class PreferencesConstants {
+
+    public static final String PREFS_NAME_USER = "prefs_user";
+    public static final String API_MD5 = "api_md5";
+    public static final String USER_ID = "user_id";
+
+}

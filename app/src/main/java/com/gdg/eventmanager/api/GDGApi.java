4372f5ed781b2a79277bf038d6ac48cae4eada92
atilabraga <braga.atila@gmail.com>
2017-04-28 01:06:15 -0300

    Adicionando okhttp logging

--- app/src/main/java/com/gdg/eventmanager/api/GDGApi.java
+++ app/src/main/java/com/gdg/eventmanager/api/GDGApi.java
@@ -6,12 +6,10 @@ import android.content.SharedPreferences;
 import com.gdg.eventmanager.model.User;
 import com.gdg.eventmanager.util.PreferencesConstants;
 
-import java.io.IOException;
-
-import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Call;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -33,44 +31,41 @@ public class GDGApi {
     private GDGApi(Context context) {
         mPreferences = context.getSharedPreferences(PreferencesConstants.PREFS_NAME_USER,
                 Context.MODE_PRIVATE);
-        OkHttpClient client = new OkHttpClient().newBuilder().addInterceptor(new Interceptor() {
-            @Override
-            public Response intercept(Chain chain) throws IOException {
-                Request request = chain.request();
+        
+        HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
+        logging.setLevel(HttpLoggingInterceptor.Level.BASIC);
+        OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                .addInterceptor(logging)
+                .addInterceptor(chain -> {
+                    Request request = chain.request();
 
-                if (mPreferences.contains(PreferencesConstants.API_MD5)
-                        && mPreferences.contains(PreferencesConstants.USER_ID)) {
-                    int userId = mPreferences.getInt(PreferencesConstants.USER_ID, 0);
-                    String apiMd5 = mPreferences.getString(PreferencesConstants.API_MD5, "");
+                    if (mPreferences.contains(PreferencesConstants.API_MD5)
+                            && mPreferences.contains(PreferencesConstants.USER_ID)) {
+                        int userId = mPreferences.getInt(PreferencesConstants.USER_ID, 0);
+                        String apiMd5 = mPreferences.getString(PreferencesConstants.API_MD5, "");
 
-                    Request.Builder requestBuilder = request.newBuilder()
-                            .addHeader(X_API_USER_ID, String.valueOf(userId))
-                            .addHeader(X_API_TOKEN, apiMd5);
-                    if (mPreferences.contains(PreferencesConstants.X_TOKEN)) {
-                        String xToken = mPreferences.getString(PreferencesConstants.X_TOKEN, "");
-                        requestBuilder
-                                .addHeader(X_TOKEN, xToken)
-                                .build();
-                    } else {
-                        requestBuilder.build();
+                        Request.Builder requestBuilder = request.newBuilder()
+                                .addHeader(X_API_USER_ID, String.valueOf(userId))
+                                .addHeader(X_API_TOKEN, apiMd5);
+                        if (mPreferences.contains(PreferencesConstants.X_TOKEN)) {
+                            String xToken = mPreferences.getString(PreferencesConstants.X_TOKEN, "");
+                            requestBuilder
+                                    .addHeader(X_TOKEN, xToken)
+                                    .build();
+                        } else {
+                            requestBuilder.build();
+                        }
                     }
-                }
 
-                Response response = chain.proceed(request);
-                return response;
-            }
-        }).build();
-
-//        HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
-//        logging.setLevel(Level.BASIC);
-//        OkHttpClient client = new OkHttpClient.Builder()
-//                .addInterceptor(logging)
-//                .build();
+                    Response response = chain.proceed(request);
+                    return response;
+                })
+                .build();
 
         Retrofit mRetrofit = new Retrofit.Builder()
                 .baseUrl(BASE_URL)
                 .addConverterFactory(GsonConverterFactory.create())
-                .client(client)
+                .client(okHttpClient)
                 .build();
         mService = mRetrofit.create(GDGService.class);
     }
